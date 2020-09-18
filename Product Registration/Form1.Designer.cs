6c4adac4e066573f2454ce6ff0de54a1e8f8a9b4
Nelson Acosta <59804314+Neacosta@users.noreply.github.com>
2020-09-18 09:17:04 -0700

    Update Form1.Designer.cs

--- Product Registration/Form1.Designer.cs
+++ Product Registration/Form1.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+       
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+       
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +17,7 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+       
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
